7aa6ef3fa683f2027adb68eb86397ec9251923df
genialingele <ingelegenial@gmail.com>
2024-12-15 22:51:11 +0200

    INIT React initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./Header";
+
+function App(){
+  return(
+    <>
+    <Header/>
+    
+    <h1>Hi</h1>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+
+  );
+
+
+}
+
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+
+function Header() {
+    return(
+        <>
+        {/*MENU*/}
+        <div className="menu">
+            <a href="#">Home </a>
+            <a href="#">Projects </a>
+            <a href="#">Experience </a>
+            <a href="#">Articles </a>
+            <a href="#">Contact </a>            
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </>
+
+
+
+
+    );
+    
+}
+
+export default Header;
