918feee639ed19a39d658baaf5ccabe2a57ef8db
genialingele <ingelegenial@gmail.com>
2024-12-23 14:50:18 +0200

    add social media links footer fix date for article

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
-function Footer(){
+function Footer() {
 
-    return(
+    return (
         <>
-        <footer >
-            <p> Genial Ingele &copy; 2024. I am a business-minded, cybersecurity-minded, 
-                full stack developer and researcher who uses creativity and logic to resolve real world problems.
-                I have years of experience in software and web development, graphic design, and marketing. 
-                I have written articles that will definitely catch your eye.
-                I am on LinkedIn, Twitter, ResearchGate, and Ndolena. </p>
-        </footer>
+            <footer >
+                <p> Genial Ingele &copy; 2024. I am a business-minded, cybersecurity-minded,
+                    full stack developer and researcher who uses creativity and logic to resolve real world problems.
+                    I have years of experience in software and web development, graphic design, and marketing.
+                    I have written articles that will definitely catch your eye.
+                    I am on LinkedIn, Twitter, ResearchGate, and Ndolena. </p>
+                <div className="social_media_icons">
+
+                    <a href="https://github.com/genialingele" className="fa fa-github" target="_blank"></a>
+                    <a href="https://twitter.com/GenialIngele" className="fa fa-twitter" target="_blank"></a>
+                    <a href="https://www.linkedin.com/in/genial-ingele/" className="fa fa-linkedin" target="_blank"></a>
+                    <a href="mailto:genial@ndolena.com" className="fa fa-envelope" target="_blank"></a>
+
+                </div>
+            </footer>
 
         </>
     );
