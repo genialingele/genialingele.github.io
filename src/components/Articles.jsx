f256784dd5772985e285a164af4f51b03deb992b
genialingele <ingelegenial@gmail.com>
2024-12-23 14:37:09 +0200

    Add more profiles Behance Artstation and add bg for exp div

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -24,13 +24,27 @@ function Articles() {
                 <section>
                     <h2>My Certifications</h2>
                     <div className="certs">
-                    {
-                        data.certs_img.map((cert, id) => (
-                            <img title={cert.title} alt={cert.title} key={id} src={cert.url}></img>
+                        {
+                            data.certs_img.map((cert, id) => (
+                                <img title={cert.title} alt={cert.title} key={id} src={cert.url}></img>
 
-                        ))
-                    }
-                    
+                            ))
+                        }
+
+                    </div>
+                    <div className="moreProfiles">
+                        <h2>My other profiles </h2>
+                        <a href="https://ingelegenial.myportfolio.com/" target="_blank" title="Behance profile for Graphic Designs">
+                            <span className="fa fa-behance"></span> Behance</a>
+                        <br></br>
+                        <a href="https://soundcloud.com/genial-ingele" target="_blank" title="Soundcloud profile for Music Production">
+                            <span className="fa fa-soundcloud"></span> SoundCloud</a>
+                        <a href="https://www.artstation.com/genialingele" target="_blank" title="Artstation profile for Arts">
+                        <br></br>
+                            <span className="fa bi-wrench-adjustable-circle"></span> ArtStation</a>
+
+                        
+
                     </div>
 
                 </section>
